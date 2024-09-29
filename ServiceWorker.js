7424ebb2e4540b7ab42215062cef51659d1733bb
OneProg Games <jade.weirdo@gmail.com>
2024-09-29 20:18:36 +0300

    0.11

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "OneProg Games-Тёмная тайга-0.11";
+const contentToCache = [
+    "Build/The-Dark-Taiga-Web.loader.js",
+    "Build/The-Dark-Taiga-Web.framework.js",
+    "Build/The-Dark-Taiga-Web.data",
+    "Build/The-Dark-Taiga-Web.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
